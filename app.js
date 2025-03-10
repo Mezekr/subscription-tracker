6ee51f9bb70135bfad966b9a5b7f7320c30a41c1
Mezekr W <mezi.edu@tuta.io>
2025-03-10 13:21:05 +0100

    add an app endpoint route for the AUTH routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import authRouter from './routes/auth.router.js';
 
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the Subscription API!');
