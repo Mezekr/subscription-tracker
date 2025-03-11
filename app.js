ed3eb62e5f9691e3834e6e756c0354f364768a21
Mezekr W <mezi.edu@tuta.io>
2025-03-11 12:54:41 +0100

    add users and subscriptions Routers into app

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 import authRouter from './routes/auth.router.js';
+import subscriptionRouter from './routes/subscription.router.js';
+import userRouter from './routes/user.router.js';
 
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the Subscription API!');
