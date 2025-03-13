a1fc41cb3f8249ff0ca095174e8f3931313ed2c8
Mezekr W <mezi.edu@tuta.io>
2025-03-13 14:38:29 +0100

    create custom mongoose errors handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/dbConfig.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 import authRouter from './routes/auth.router.js';
 import subscriptionRouter from './routes/subscription.router.js';
 import userRouter from './routes/user.router.js';
@@ -11,6 +12,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+app.use(errorMiddleware);
 app.get('/', (req, res) => {
 	res.send('Welcome to the Subscription API!');
 });
