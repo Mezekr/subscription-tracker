7c0e71e44cad5cfd8ff4183563bb85f9468834c7
Mezekr W <mezi.edu@tuta.io>
2025-03-13 14:49:55 +0100

    add common express middlewares

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/dbConfig.js';
@@ -7,6 +8,10 @@ import subscriptionRouter from './routes/subscription.router.js';
 import userRouter from './routes/user.router.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
