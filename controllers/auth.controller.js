61f7f7538696ef8f93841cc1a225f014214b6f6f
Mezekr W <mezi.edu@tuta.io>
2025-03-16 12:04:29 +0100

    create auth controllers for auth routers

---
+++ controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signUp = (req, res, next) => {
+	res.send({ title: 'Sign up' });
+};
+export const signIn = (req, res, next) => {
+	res.send({ title: 'Sign in' });
+};
+export const signOut = (req, res, next) => {
+	res.send({ title: 'Sign out' });
+};
