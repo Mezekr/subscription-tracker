dd99a0566b04a53285e906ca75e67cc75eb5756a
Mezekr W <mezi.edu@tuta.io>
2025-03-16 12:01:13 +0100

    export JWT secret key and expiry period constants

--- config/env.js
+++ config/env.js
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URL } = process.env;
+export const { PORT, NODE_ENV, DB_URL, JWT_SECRET_KEY, JWT_EXPIRES_IN } =
+	process.env;
