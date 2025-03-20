83f02e9888bb08cf37a63b49357be704bd09955f
Mezekr W <mezi.edu@tuta.io>
2025-03-20 17:45:53 +0100

    implement user sign-in controller handler

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const signUp = async (req, res, next) => {
 		);
 
 		// 2.3 Sign users with JWT for authentication
-		const token = jwt.sign({ userI: newUsers[0]._id }, JWT_SECRET_KEY, {
+		const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET_KEY, {
 			expiresIn: JWT_EXPIRES_IN,
 		});
 
@@ -54,8 +54,41 @@ export const signUp = async (req, res, next) => {
 		next(error);
 	}
 };
-export const signIn = (req, res, next) => {
-	res.send({ title: 'Sign in' });
+export const signIn = async (req, res, next) => {
+	try {
+		// 1. Get user login data
+		const { email, password } = req.body;
+		// 2. check if user already exits
+		const user = await User.findOne({ email });
+		if (!user) {
+			const error = new Error('User not found');
+			error.statusCode = 404;
+			throw error;
+		}
+		// 3. compare password
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			const error = new Error('Invalid password');
+			error.statusCode = 401;
+			throw error;
+		}
+		// 4. Generate a token
+		const token = await jwt.sign({ userId: user._id }, JWT_SECRET_KEY, {
+			expiresIn: JWT_EXPIRES_IN,
+		});
+		// 5. Send response
+		res.status(200).json({
+			success: true,
+			message: ' User signed successfully',
+			data: {
+				token,
+				user,
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 };
 export const signOut = (req, res, next) => {
 	res.send({ title: 'Sign out' });
