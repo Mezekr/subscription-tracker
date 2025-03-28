77a3c1f56d57df46d0eba473035424fccf7eec42
Mezekr W <mezi.edu@tuta.io>
2025-03-28 14:27:05 +0100

    set arcjet key and environment variable

--- config/env.js
+++ config/env.js
@@ -2,5 +2,12 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URL, JWT_SECRET_KEY, JWT_EXPIRES_IN } =
-	process.env;
+export const {
+	PORT,
+	NODE_ENV,
+	DB_URL,
+	JWT_SECRET_KEY,
+	JWT_EXPIRES_IN,
+	ARCJET_KEY,
+	ARCJET_ENV,
+} = process.env;
