5d0effcaa9bf1e0a9db237c956b340762f9cd4cd
Mezekr W <mezi.edu@tuta.io>
2025-03-30 12:47:50 +0200

    implement get user subscriptions controller handler

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -7,7 +7,7 @@ export const createSubscription = async (req, res, next) => {
 			user: req.user._id,
 		});
 		res.status(201).json({
-			successes: true,
+			success: true,
 			message: 'Subscription successfully created',
 			data: {
 				subscription,
@@ -15,6 +15,27 @@ export const createSubscription = async (req, res, next) => {
 		});
 	} catch (error) {
 		console.error('Failed to create a subscription');
+		next(error);
+	}
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+	try {
+		//check the User with user on the token
+		if (req.user.id !== req.params.id) {
+			const err = new Error('You are not the owner of this account');
+			err.status = 401;
+			throw err;
+		}
+		const subscriptions = await Subscription.find({ user: req.params.id });
+
+		res.status(200).json({
+			success: true,
+			message: 'Subscriptions successfully retrieved',
+			data: subscriptions,
+		});
+	} catch (error) {
+		console.error(error.message);
 		next(error);
 	}
 };
