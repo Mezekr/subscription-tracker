8d027b8d9d795a21322fdf74bc08b08f84bbe2f6
Mezekr W <mezi.edu@tuta.io>
2025-03-31 20:20:47 +0200

    add upstash workflow router to app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 import authRouter from './routes/auth.router.js';
 import subscriptionRouter from './routes/subscription.router.js';
 import userRouter from './routes/user.router.js';
+import workflowRouter from './routes/workflow.router.js';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(arcjetMiddleware);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/app/v1/workflows', workflowRouter);
 
 app.use(errorMiddleware);
 app.get('/', (req, res) => {
