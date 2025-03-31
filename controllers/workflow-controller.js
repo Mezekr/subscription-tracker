8cbcf29a8f9562e1e3eae1868ce12625a774b35f
Mezekr W <mezi.edu@tuta.io>
2025-03-31 20:34:59 +0200

    implement subscription remainder impossible cases

--- controllers/workflow-controller.js
+++ controllers/workflow-controller.js
@@ -1,11 +1,21 @@
 import { serve } from '@upstash/workflow/express';
-import Subscription from './../models/subscription.model';
+import dayjs from 'dayjs';
+import Subscription from '../models/subscription.model';
 
 export const sendRemainder = serve(async (context) => {
 	const { subscriptionId } = context.requestPayload;
 	const subscription = await fetchSubscription(context, subscriptionId);
 
 	if (!subscription || subscription.status === 'inactive') return;
+
+	const renewalDate = dayjs(Subscription.renewalDate);
+
+	if (renewalDate.isBefore(dayjs())) {
+		console.log(
+			`Renewal date has passed for the Subscription ${subscriptionId}. Stopping workflow`
+		);
+		return;
+	}
 });
 
 const fetchSubscription = async (context, subscriptionId) => {
