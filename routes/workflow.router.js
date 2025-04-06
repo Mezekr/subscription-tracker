cf34457e4c4954dbb8a419bc0ece1d8d17dd970e
Mezekr W <mezi.edu@tuta.io>
2025-04-06 13:28:51 +0200

    add workflow endpoint router handler

--- routes/workflow.router.js
+++ routes/workflow.router.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { sendRemainder } from '../controllers/workflow-controller.js';
 
 const workflowRouter = Router();
 
-workflowRouter.get('/', (req, res, next) => {});
+workflowRouter.post('/subscription/remainder', sendRemainder);
 
 export default workflowRouter;
