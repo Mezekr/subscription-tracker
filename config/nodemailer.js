2fcea97d56f08f62cbbdceb844ef5a8b9795b334
Mezekr W <mezi.edu@tuta.io>
2025-04-19 13:29:37 +0200

    create send mail transport config for gmail

---
+++ config/nodemailer.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer';
+import { EMAIL_ADDRESS, EMAIL_PASSWORD } from './env.js';
+
+export const accountEmail = EMAIL_ADDRESS;
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: accountEmail,
+		pass: EMAIL_PASSWORD,
+	},
+});
+
+export default transporter;
